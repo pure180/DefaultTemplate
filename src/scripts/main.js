20109d68f7a0badce78fee7d0b84ab29a5f5b68b
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-02-27 09:20:14 +0100

    rebuild template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
+(function($) {
 
+  $.fn.fullWidthBgImg = function(options){
+      var settings = {
+      };
+
+      var o = $.extend({}, settings, options);
+
+      var $this = $( this ).find('.jumbotron-fullwidthimg');
+
+      var idHeight;
+      var idWidth;
+
+      function getHeight(height) {
+        var height = $(window).height();
+        var navHeight = $('body').find('.navbar').height();
+
+        $this.css('height', height - navHeight );
+        console.log(height);
+      };
+
+      function getWidth(width) {
+        var width = $this.width()
+        console.log(width);
+      };
+
+
+      $(window).resize( function() {
+        clearTimeout(idHeight, idWidth);
+        idHeight = setTimeout(getHeight, 500);
+        idWidth = setTimeout(getWidth, 500);
+      });
+
+      return getWidth(), getHeight();
+  };
+
+})(jQuery);
