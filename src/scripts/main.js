4a34e462b541d2a766cc9b32e189b10d56b04908
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-10-09 15:21:56 +0200

    old changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,41 @@
-(function($) {
+(function(){
+  "use strict";
+  (function($) {
 
-  $.fn.fullWidthBgImg = function(options){
-      var settings = {
-      };
+    $.fn.toggleNav = function(options){
+        var settings = {
+          btn: '.sidenav',
+          wrap: '.sidenavwrap',
+          easing: 'easeOutCirc',
+          ClassToToggle: 'open',
+          duration: 300
+        };
 
-      var o = $.extend({}, settings, options);
+        var o = $.extend({}, settings, options);
 
-      var $this = $( this ).find('.jumbotron-fullwidthimg');
+        var $this = $(this);
+        var $btn = $this.find(o.btn);
+        var $wrap = $this.find(o.wrap);
 
-      var idHeight;
-      var idWidth;
+        function toggleWrap(event) {
+          event.stopPropagation();
+          $wrap.toggle(o.duration, o.easing, function(){
+            $(this).toggleClass( o.ClassToToggle, function(){
+            });
+          });
+        }
 
-      function getHeight(height) {
-        var height = $(window).height();
-        var navHeight = $('body').find('.navbar').height();
-
-        $this.css('height', height - navHeight );
-        console.log(height);
-      };
-
-      function getWidth(width) {
-        var width = $this.width()
-        console.log(width);
-      };
+        function hideWrap(){
+          $wrap.hide(o.duration, o.easing,function() {});
+        }
 
 
-      $(window).resize( function() {
-        clearTimeout(idHeight, idWidth);
-        idHeight = setTimeout(getHeight, 500);
-        idWidth = setTimeout(getWidth, 500);
-      });
+          $btn.on('click', toggleWrap);
+          $('body').on('click', hideWrap);
+          //$wrap.css('position', 'absolute').hide();
 
-      return getWidth(), getHeight();
-  };
 
-})(jQuery);
+    };
+
+  })(jQuery);
+})();
