2a746d98917a0cfa2640f2d1bd5713e4017e08d1
Daniel Pfisterer <info@daniel-pfisterer.de>
2016-04-19 23:28:14 +0200

    add log to gitignore

--- gulp_task/jade.js
+++ gulp_task/jade.js
@@ -1,32 +1,24 @@
 'use-strict';
 var gulp = require('gulp');
-var jade = require('gulp-jade');
+var pug = require('gulp-pug');
 var changed = require('gulp-changed');
-var cached = require('gulp-cached');
-var gulpif = require('gulp-if');
-var filter = require('gulp-filter');
 var notify = require('gulp-notify');
 var gutil = require('gulp-util');
-var jadeInheritance = require('gulp-jade-inheritance');
 var plumber = require('gulp-plumber');
 
 module.exports = function (gulp, plugins, path) {
   return function () {
     var jade_task = function(src, dist, note){
       return gulp.src( src )
-        //.pipe(changed(path.dist.html, {extension: '.html'}))
         .pipe(plumber(function (error) {
             gutil.log(error.message);
             this.emit('end');
         }))
-        //.pipe(gulpif(global.isWatching, cached('jade')))
-        //.pipe(jadeInheritance({basedir: path.src.jade }))
-        //.pipe(filter(function (file) {
-        //  return !/\/_/.test(file.path) || !/^_/.test(file.relative);
-        //}))
-        .pipe(jade({
-          pretty: true
-        }))
+        .pipe(
+          pug({
+            pretty: true
+          })
+        )
         .pipe(gulp.dest( dist ))
         .pipe(notify({
           message: note
